b1599ae4f131422ef11d9e9c84de922c368e69d4
zeolee01 <114869179+zeolee01@users.noreply.github.com>
2024-02-08 23:36:31 +0530

    navbar change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./App.css"
+import { Navbar } from "./Components/Navbar/Navbar"
+import "bootstrap/dist/css/bootstrap.css"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+      <Navbar />
     </>
   )
 }
